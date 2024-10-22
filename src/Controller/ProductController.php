ae6e466d23803ba593edd31a3208a26615a72e1f
DESPREZ BASTIEN p2202099 <bastien.desprez@etu.univ-lyon1.fr>
2024-10-22 12:10:07 +0200

    ImageProduct

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -12,10 +12,10 @@ class ProductController extends AbstractController
     public function index(): Response
     {
         $products = [
-            ['id' => 1, 'name' => 'Processeur Intel Core i7', 'price' => 349.99, 'image' => 'cpu.jpg', 'type' => 'cpu'],
-            ['id' => 2, 'name' => 'Carte graphique NVIDIA RTX 3080', 'price' => 699.99, 'image' => 'gpu.jpg', 'type' => 'gpu'],
-            ['id' => 3, 'name' => 'SSD Samsung 1To', 'price' => 129.99, 'image' => 'ssd.jpg', 'type' => 'ssd'],
-            ['id' => 4, 'name' => 'Carte mère ASUS ROG', 'price' => 249.99, 'image' => 'motherboard.jpg', 'type' => 'motherboard'],
+            ['id' => 1, 'name' => 'Processeur Intel Core i7', 'price' => 349.99, 'image' => 'i7.png', 'type' => 'cpu'],
+            ['id' => 2, 'name' => 'Carte graphique NVIDIA RTX 3080', 'price' => 699.99, 'image' => 'rtx.jpg', 'type' => 'gpu'],
+            ['id' => 3, 'name' => 'SSD Samsung 1To', 'price' => 129.99, 'image' => 'ssd.avif', 'type' => 'ssd'],
+            ['id' => 4, 'name' => 'Carte mère ASUS ROG', 'price' => 249.99, 'image' => 'motherboard.png', 'type' => 'motherboard'],
         ];
 
         return $this->render('product/index.html.twig', [
