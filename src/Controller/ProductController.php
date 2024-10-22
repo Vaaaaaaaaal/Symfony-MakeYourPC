d381726f78165dc00e09d5c3a2d03d644e87ce3b
DESPREZ BASTIEN p2202099 <bastien.desprez@etu.univ-lyon1.fr>
2024-10-22 11:49:14 +0200

    uiOk

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    #[Route('/products', name: 'app_products')]
+    public function index(): Response
+    {
+        $products = [
+            ['id' => 1, 'name' => 'Processeur Intel Core i7', 'price' => 349.99, 'image' => 'cpu.jpg', 'type' => 'cpu'],
+            ['id' => 2, 'name' => 'Carte graphique NVIDIA RTX 3080', 'price' => 699.99, 'image' => 'gpu.jpg', 'type' => 'gpu'],
+            ['id' => 3, 'name' => 'SSD Samsung 1To', 'price' => 129.99, 'image' => 'ssd.jpg', 'type' => 'ssd'],
+            ['id' => 4, 'name' => 'Carte mère ASUS ROG', 'price' => 249.99, 'image' => 'motherboard.jpg', 'type' => 'motherboard'],
+        ];
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products,
+        ]);
+    }
+}
