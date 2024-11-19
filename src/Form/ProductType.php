c6d6bc8bc9162c2e1c4f788eec62d0700fc27d3b
valentin <valentin.goux@etu.univ-lyon1.fr>
2024-11-19 16:36:50 +0100

    add/edit product type

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -3,12 +3,16 @@
 namespace App\Form;
 
 use App\Entity\Product;
+use App\Entity\Type;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\NumberType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
 
 class ProductType extends AbstractType
 {
@@ -16,18 +20,49 @@ class ProductType extends AbstractType
     {
         $builder
             ->add('name', TextType::class, [
-                'label' => 'Nom du produit'
+                'label' => 'Nom du produit',
+                'attr' => [
+                    'placeholder' => 'Entrez le nom du produit'
+                ]
             ])
-            ->add('price', NumberType::class, [
-                'label' => 'Prix'
+            ->add('price', MoneyType::class, [
+                'label' => 'Prix',
+                'currency' => 'EUR',
+                'attr' => [
+                    'placeholder' => 'Entrez le prix'
+                ]
             ])
             ->add('stock', NumberType::class, [
-                'label' => 'Stock'
+                'label' => 'Stock disponible',
+                'attr' => [
+                    'placeholder' => 'Entrez la quantité en stock'
+                ]
             ])
             ->add('image', FileType::class, [
-                'label' => 'Image',
+                'label' => 'Image du produit',
+                'mapped' => false,
                 'required' => false,
-                'mapped' => false
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                            'image/webp'
+                        ],
+                        'mimeTypesMessage' => 'Veuillez uploader une image valide (JPEG, PNG, WEBP)',
+                    ])
+                ],
+            ])
+            ->add('type', EntityType::class, [
+                'class' => Type::class,
+                'choice_label' => 'name',
+                'label' => 'Type de produit',
+                'placeholder' => 'Sélectionnez un type',
+                'required' => true,
+                'attr' => [
+                    'class' => 'form-select'
+                ]
             ])
         ;
     }
