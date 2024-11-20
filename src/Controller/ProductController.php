d2b9118bbcb5ea565ecf377b114228eebf7b67eb
valentin <valentin.goux@etu.univ-lyon1.fr>
2024-11-20 08:38:10 +0100

    css edit product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Form\ProductType;
 
 class ProductController extends AbstractController
 {
